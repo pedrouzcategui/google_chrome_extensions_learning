00f3a9a2084843f3b0b4dfc477f9b43a308e2318
Pedro <pedro.porfirio@ingeniust.net>
2023-11-30 00:20:53 -0400

    Reading Time Application

---
+++ learning/manifest.md
@@ -0,0 +1,7 @@
+To learn more about manifests, go here: https://developer.chrome.com/docs/extensions/mv3/manifest/
+
+Use PNG when posible, not SVG.
+
+## What is the content script?
+
+Scripts that read and modify the contents of a page. Content Scripts are Isolated, in their own context without interferring with the host page or other extension content scripts.
---
+++ tutorials/reading_time/scripts/content.js
@@ -0,0 +1,26 @@
+const article = document.querySelector("article");
+
+// `document.querySelector` may return null if the selector doesn't match anything.
+if (article) {
+  const text = article.textContent;
+  const wordMatchRegExp = /[^\s]+/g; // Regular expression
+  const words = text.matchAll(wordMatchRegExp);
+  // matchAll returns an iterator, convert to array to get word count
+  const wordCount = [...words].length;
+  const readingTime = Math.round(wordCount / 200);
+  const badge = document.createElement("p");
+  // Use the same styling as the publish information in an article's header
+  badge.classList.add("color-secondary-text", "type--caption");
+  badge.textContent = `⏱️ ${readingTime} min read`;
+
+  // Support for API reference docs
+  const heading = article.querySelector("h1");
+  // Support for article docs with date
+  const date = article.querySelector("time")?.parentNode;
+
+  (date ?? heading).insertAdjacentElement("afterend", badge);
+}
+
+// To Dos:
+// 1. Add another match pattern in the manifest.json to support other chrome developer pages, like for example, the Chrome DevTools or Workbox.
+// 2. Add a new content script that calculates the reading time to any of your favorite blogs or documentation sites.
